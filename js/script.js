a011bdd7dd609b039300f3453399a4561cbe8eac
andreacassini <andrea1316@hotmail.it>
2023-05-29 20:03:38 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//FUNZIONE CHE CREA QUADRATI ALL'INTERNO DELLA GRID
+function squareElem(){
+    let square = document.createElement('div');
+    square.classList.add('square')
+    return square;
+}
+
+const button = document.getElementById('start').addEventListener('click', function(){
+    
+})
+//COLLEGAMENTO GRID NEL DOM
+const grid = document.getElementById('grid');
+
+//CICLO PER CREARE LA TABELLA
+for(let i=0; i<100; i++){
+    let square = squareElem();
+    square.innerText = i + 1;
+    square.addEventListener('click',function(){
+        this.classList.add('clicked');
+        console.log('Casella n.'+square.innerText)
+
+    })
+    grid.append(square);
+}
